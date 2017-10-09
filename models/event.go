354e2dc9a11725216579f0fe0655686d7b9932a1
Paweł Nowak <pavelvaremreis@gmail.com>
2017-10-09 18:21:24 +0200

    Add start and end date to event model

--- models/event.go
+++ models/event.go
@@ -1,7 +1,11 @@
 package models
 
+import "time"
+
 type Event struct {
-	Id    string   `json:"id"`
-	Title   string   `json:"title"`
-	Description   string `json:"description"`
+	Id string `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	StartDate time.Time `json:"startDate"`
+	EndDate time.Time `json:"endDate"`
 }
