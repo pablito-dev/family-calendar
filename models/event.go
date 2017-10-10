f28ccc9e4e3c8375935999985436f17cbf9c4d47
Paweł Nowak <pavelvaremreis@gmail.com>
2017-10-10 23:17:05 +0200

    Add user to event struct

--- models/event.go
+++ models/event.go
@@ -6,6 +6,7 @@ type Event struct {
 	Id string `json:"id"`
 	Title string `json:"title"`
 	Description string `json:"description"`
+	User string `json:"user"`
 	StartDate time.Time `json:"startDate"`
 	EndDate time.Time `json:"endDate"`
 }
